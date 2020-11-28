87cea0d720fe5c819ff39980e863cb1d439c2ff7
cowdingus <zaki.nadhif45@gmail.com>
2020-11-28 12:10:38 +0700

    Refactored code

--- code/Application/UserInterface.cpp
+++ code/Application/UserInterface.cpp
@@ -1,18 +1,38 @@
 #include "Application/UserInterface.hpp"
 #include <TGUI/Exception.hpp>
 
-UserInterface::UserInterface() {}
-
 UserInterface::UserInterface(tgui::Group::Ptr container)
 	: container(container)
 {
-	auto topBar = tgui::Group::create();
-	topBar->loadWidgetsFromFile("assets/forms/topBar.txt");
-	container->add(topBar, "topBar");
+	auto topPanelContainer = tgui::Group::create();
+	topPanelContainer->loadWidgetsFromFile("assets/forms/topPanel.txt");
+	container->add(topPanelContainer, "topPanelContainer");
 
-	auto bottomBar = tgui::Group::create();
-	bottomBar->loadWidgetsFromFile("assets/forms/bottomBar.txt");
-	container->add(bottomBar, "bottomBar");
+	auto bottomPanelContainer = tgui::Group::create();
+	bottomPanelContainer->loadWidgetsFromFile("assets/forms/bottomPanel.txt");
+	container->add(bottomPanelContainer, "bottomPanelContainer");
+
+	auto middlePanelContainer = tgui::Group::create();
+	middlePanelContainer->setPosition("0", "topPanelContainer.topPanel.bottom");
+	middlePanelContainer->setSize("100%", "bottomPanelContainer.bottomPanel.top - topPanelContainer.topPanel.bottom");
+	container->add(middlePanelContainer, "middlePanelContainer");
+
+	pageManager = new PageManager(middlePanelContainer);
+
+	auto reloadPageButton = tgui::Button::create("R");
+	reloadPageButton->setPosition("50% - width / 2", "0");
+	reloadPageButton->connect("pressed", 
+		[this](){ pageManager->reloadPages(); }
+	);
+	container->add(reloadPageButton, "reloadPageButton");
+
+	pageManager->addPage("assets/forms/emptyPage.txt", "emptyPage");
+	pageManager->setActivePage("emptyPage");
+}
+
+UserInterface::~UserInterface()
+{
+	delete pageManager;
 }
 
 
