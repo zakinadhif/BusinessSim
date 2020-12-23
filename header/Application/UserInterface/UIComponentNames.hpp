9a418f9c4cb939fb411a900b73fd8ee961575254
cowdingus <zaki.nadhif45@gmail.com>
2020-12-23 13:24:32 +0700

    Added UI components name library

---
+++ header/Application/UserInterface/UIComponentNames.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <string>
+#include <string_view>
+
+namespace UIComponentNames
+{
+	constexpr char SWITCH_TRADING_MENU_BUTTON[] = "tradingButton";
+	constexpr char SWITCH_COMMODITIES_MENU_BUTTON[] = "commoditiesButton";
+	constexpr char SWITCH_SOCIAL_MENU_BUTTON[] = "socialButton";
+
+	constexpr char MONEY_LABEL[] = "moneyLabel";
+	constexpr char TICKET_LABEL[] = "ticketLabel";
+
+	constexpr char STEP_BUTTON[] = "stepButton";
+	constexpr char STATS_BUTTON[] = "statsButton";
+};
