620019103f5ebe394e4bf6a4557c97d56a947024
cowdingus <zaki.nadhif45@gmail.com>
2020-12-29 17:30:44 +0700

    Updated componentNames

--- header/Application/UserInterface/UIComponentNames.hpp
+++ header/Application/UserInterface/UIComponentNames.hpp
@@ -15,9 +15,15 @@ namespace UIComponentNames
 	constexpr char STEP_BUTTON[] = "stepButton";
 	constexpr char STATS_BUTTON[] = "statsButton";
 
+	constexpr char STOCK_ROOT[] = "stockItem";
+	constexpr char STOCK_BUY_COUNT_SPIN[] = "buyCountSpin";
+	constexpr char STOCK_BUY_COUNT_LABEL[] = "buyCountLabel";
+	constexpr char STOCK_COMPANY_NAME[] = "companyName";
 	constexpr char STOCK_LAST_PRICE_LABEL[] = "lastPriceLabel";
 	constexpr char STOCK_OPEN_PRICE_LABEL[] = "openPriceLabel";
 	constexpr char STOCK_CLOSE_PRICE_LABEL[] = "closePriceLabel";
 	constexpr char STOCK_HIGH_PRICE_LABEL[] = "highPriceLabel";
 	constexpr char STOCK_LOW_PRICE_LABEL[] = "lowPriceLabel";
 };
+
+
