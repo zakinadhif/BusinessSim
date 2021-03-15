7ad17ea5ecde7db12f4d8a0b2f0fc21afe39049c
cowdingus <zaki.nadhif45@gmail.com>
2021-03-15 10:12:40 +0700

    Refactored PriceSimulator

--- code/Application/Market/PriceSimulator.cpp
+++ code/Application/Market/PriceSimulator.cpp
@@ -1,61 +1,52 @@
 #include "Application/Market/PriceSimulator.hpp"
-#include "Application/Market/MarketDetails.hpp"
 #include "Application/Market/PriceQuote.hpp"
 
 #include <random>
-#include <spdlog/spdlog.h>
 
 #include <cmath>
 
-PriceSimulator::PriceSimulator(float initialPrice, MarketDetails marketDetails, int tradingPeriod)
-	: tradingPeriod(tradingPeriod)
+PriceSimulator::PriceSimulator(float initialPrice, float drift, float volatility, int tradingPeriod)
+	: m_tradingPeriod{tradingPeriod}
+	, m_price{initialPrice}
+	, m_drift{drift}
+	, m_volatility{volatility}
 {
-	setPrice(initialPrice);	
-	setDrift(marketDetails.drift);
-	setVolatility(marketDetails.volatility);
 }
 
 void PriceSimulator::step()
 {
-	float growth = std::exp(drift + volatility * stdRandom());
-	price = price * std::pow(growth, 1 / static_cast<float>(tradingPeriod));
-
-	++stepCount;
-
-	if (stepCount == tradingPeriod)
-	{
-		stepCount = 0;
-	}
+	float growth = std::exp(m_drift + m_volatility * boxMullerRandom());
+	m_price = m_price * std::pow(growth, 1 / static_cast<float>(m_tradingPeriod));
 }
 
 void PriceSimulator::setPrice(float price)
 {
-	this->price = price;
+	m_price = price;
 }
 
 void PriceSimulator::setDrift(float drift)
 {
-	this->drift = drift;
+	m_drift = drift;
 }
 
 void PriceSimulator::setVolatility(float volatility)
 {
-	this->volatility = volatility;
+	m_volatility = volatility;
 }
 
 float PriceSimulator::getPrice() const
 {
-	return price;
+	return m_price;
 }
 
 float PriceSimulator::getDrift() const
 {
-	return drift;
+	return m_drift;
 }
 
 float PriceSimulator::getVolatility() const
 {
-	return volatility;
+	return m_volatility;
 }
 
 float PriceSimulator::boxMullerRandom()
