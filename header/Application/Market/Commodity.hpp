fbc8c167e93152d9ec636f51108652ebe65d5e0e
cowdingus <56494343+cowdingus@users.noreply.github.com>
2021-03-21 22:18:19 +0700

    Merge CommodityData struct to Commodity

--- header/Application/Market/Commodity.hpp
+++ header/Application/Market/Commodity.hpp
@@ -1,9 +1,20 @@
 #pragma once
 
+#include "Application/Market/MarketEvent.hpp"
+
+#include <vector>
 #include <string>
 
 struct Commodity
 {
-	std::string name;
 	float price;
+
+	const std::string shortName;
+	const std::string fullName;
+
+	const std::string description;
+	
+	const std::string logoPath;
+
+	const std::vector<MarketEvent> events;
 };
