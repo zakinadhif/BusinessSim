7793e76bebee17f5bac870733dbd3f16d9e7be2d
cowdingus <zaki.nadhif45@gmail.com>
2021-03-23 17:24:30 +0700

    Remove arrow configuration on createStockWidget

--- code/Application/WidgetFactory.cpp
+++ code/Application/WidgetFactory.cpp
@@ -15,19 +15,15 @@ namespace ResourceIDs = UIResourceIds;
 tgui::Panel::Ptr createStockWidget(Commodity& commodity, TextureHolder& textureHolder)
 {
 	auto widget = tgui::Panel::create({"100%", "75"});
-	widget->loadWidgetsFromFile(UIFormPaths::STOCK_ITEM);
+	widget->loadWidgetsFromFile(FormPaths::STOCK_ITEM);
 
 	auto companyLogoPicture = widget->get<tgui::Picture>(Components::STOCK_COMPANY_LOGO);
 	auto priceLabel = widget->get<tgui::Label>(Components::STOCK_PRICE_LABEL);
-	auto arrowPicture = widget->get<tgui::Picture>(Components::STOCK_ARROW_PICTURE);
 
 	// This expects the commodity's logo resource ID to be the same with its short name
 	auto& companyLogoTexture = textureHolder[commodity.shortName];
-	auto& noArrowStableTexture = textureHolder[ResourceIDs::NO_ARROW_STABLE];
 
 	companyLogoPicture->getRenderer()->setTexture(companyLogoTexture);
-	arrowPicture->getRenderer()->setTexture(noArrowStableTexture);
-
 	priceLabel->setText(fmt::format("{:.2f} E", commodity.price));
 
 	return widget;
