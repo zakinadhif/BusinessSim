55ae39ba68cfd0eb36b0b88f399a06243637a2a5
cowdingus <56494343+cowdingus@users.noreply.github.com>
2021-03-26 18:28:43 +0700

    Add stock company logo size scaling algorithm

--- code/Application/WidgetFactory.cpp
+++ code/Application/WidgetFactory.cpp
@@ -8,6 +8,8 @@
 #include <TGUI/Widgets/Panel.hpp>
 #include <fmt/format.h>
 
+#include <math.h>
+
 namespace Components = UIComponentNames;
 namespace FormPaths = UIFormPaths;
 namespace ResourceIDs = UIResourceIds;
@@ -18,6 +20,7 @@ tgui::Panel::Ptr createStockWidget(Commodity& commodity, TextureHolder& textureH
 	widget->loadWidgetsFromFile(FormPaths::STOCK_ITEM);
 
 	auto companyLogoPicture = widget->get<tgui::Picture>(Components::STOCK_COMPANY_LOGO);
+	auto companyLogoFrame = widget->get<tgui::Group>(Components::STOCK_COMPANY_LOGO_FRAME);
 	auto priceLabel = widget->get<tgui::Label>(Components::STOCK_PRICE_LABEL);
 
 	// This expects the commodity's logo resource ID to be the same with its short name
@@ -25,6 +28,18 @@ tgui::Panel::Ptr createStockWidget(Commodity& commodity, TextureHolder& textureH
 
 	companyLogoPicture->getRenderer()->setTexture(companyLogoTexture);
 	priceLabel->setText(fmt::format("{:.2f} E", commodity.price));
+
+	// Calculate scaling maximum scaling while preserving aspect ratio
+	sf::Vector2f companyLogoFrameSize = companyLogoFrame->getSize();
+	sf::Vector2f companyLogoTextureSize = static_cast<sf::Vector2f>(companyLogoTexture.getSize());
+
+	float maxScale = std::min(
+		companyLogoFrameSize.x / companyLogoTextureSize.x,
+		companyLogoFrameSize.y / companyLogoTextureSize.y
+	);
+
+	companyLogoPicture->setPosition({0,0});
+	companyLogoPicture->setSize(companyLogoTextureSize.x * maxScale, companyLogoTextureSize.y * maxScale);
 
 	return widget;
 }
