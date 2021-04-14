9e6ea878e2c53d0e859fd68b244d7ad4e329c496
cowdingus <zaki.nadhif45@gmail.com>
2021-04-14 11:35:45 +0700

    Turn PriceSimulator into pure function

--- code/Application/Market/PriceSimulator.cpp
+++ code/Application/Market/PriceSimulator.cpp
@@ -1,54 +1,11 @@
 #include "Application/Market/PriceSimulator.hpp"
 
+#include "Application/GameSettings.hpp"
 #include "Application/Utility/Random.hpp"
 
 #include <cmath>
 
-PriceSimulator::PriceSimulator(float initialPrice, float drift, float volatility, int tradingPeriod)
-	: m_tradingPeriod{tradingPeriod}
-	, m_price{initialPrice}
-	, m_drift{drift}
-	, m_volatility{volatility}
-{
-}
-
-void PriceSimulator::step()
-{
-	float growth = std::exp(m_drift + m_volatility * boxMullerRandom());
-	m_price = m_price * std::pow(growth, 1 / static_cast<float>(m_tradingPeriod));
-}
-
-void PriceSimulator::setPrice(float price)
-{
-	m_price = price;
-}
-
-void PriceSimulator::setDrift(float drift)
-{
-	m_drift = drift;
-}
-
-void PriceSimulator::setVolatility(float volatility)
-{
-	m_volatility = volatility;
-}
-
-float PriceSimulator::getPrice() const
-{
-	return m_price;
-}
-
-float PriceSimulator::getDrift() const
-{
-	return m_drift;
-}
-
-float PriceSimulator::getVolatility() const
-{
-	return m_volatility;
-}
-
-float PriceSimulator::boxMullerRandom()
+static float boxMullerRandom()
 {
 	float u1 = Random::getFloat();
 	float u2 = Random::getFloat();
@@ -63,4 +20,10 @@ float PriceSimulator::boxMullerRandom()
 	}
 
 	return boxMullerRandom();
+}
+
+float simulateNextPrice(float priceNow, float drift, float volatility)
+{
+	float growth = std::exp(drift + volatility * boxMullerRandom());
+	return priceNow * std::pow(growth, 1 / static_cast<float>(GameSettings::STOCK_TRADING_LENGTH));
 }
--- header/Application/Market/PriceSimulator.hpp
+++ header/Application/Market/PriceSimulator.hpp
@@ -1,33 +1,3 @@
 #pragma once
 
-#include "Application/GameSettings.hpp"
-
-class PriceSimulator
-{
-public:
-	PriceSimulator(
-		float startingPrice = 0.0f,
-		float drift = 0.0f,
-		float volatility = 0.0f,
-		int tradingPeriod = GameSettings::STOCK_TRADING_LENGTH
-	);
-
-	void setPrice(float price);
-	void setDrift(float drift);
-	void setVolatility(float volatility);
-
-	float getPrice() const;
-	float getDrift() const;
-	float getVolatility() const;
-
-	void step();
-
-private:
-	float m_price = 0.f;
-	float m_drift = 0.f;
-	float m_volatility = 0.f;
-
-	const int m_tradingPeriod = 0;
-
-	float boxMullerRandom();
-};
+float simulateNextPrice(float priceNow, float drift, float volatility);
